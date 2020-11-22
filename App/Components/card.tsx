85ed039ef34ef7c9caac9a2972255f08ad718b0d
Yassine Mhanna <yassine.mhanna@wee.com>
2020-11-23 00:02:49 +0100

    fixed android build issues

--- App/Components/card.tsx
+++ App/Components/card.tsx
@@ -1,7 +1,5 @@
 import React, {ReactElement} from 'react';
 import {View, StyleSheet, TouchableOpacity, Text} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import Icon from 'react-native-vector-icons/Ionicons';
 
 type Props = {
   title: string;
@@ -15,13 +13,6 @@ const Card = ({onPress, title, url}: Props): ReactElement => {
       <View style={styles.rssFeedContainer}>
         <Text style={styles.rssFeed}>{title}</Text>
         <Text>{url}</Text>
-      </View>
-      <View style={styles.rightIconContainer}>
-        <Icon
-          name="ios-arrow-forward-circle-outline"
-          color={Colors.weeWhite}
-          size={25}
-        />
       </View>
     </TouchableOpacity>
   );
